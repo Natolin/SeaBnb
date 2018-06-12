502f806af8f8b7b27c8e436a82ceec9951eb6913
Christian Philipp <ch.philipp@outlook.com>
2018-06-12 11:52:30 +0200

    Ready View Index and Show Boats

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,8 @@
 class UsersController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
   def update
   end
 
   def edit
-  end
-
-  def destroy
-  end
-
-  def index
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :bookings
 
   validates :email, uniqueness: true, presence: true
-  validates :user_name, uniqueness: true, presence: true
+  validates :user_name, uniqueness: true, presence: true, on: :update
   validates :password, presence: true
 
   devise :database_authenticatable, :registerable,
