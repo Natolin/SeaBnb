66d68d38457125d80bcb1928adbf3d1813905ec5
Sydney Thomas <sydneyathomas@gmail.com>
2018-06-12 10:55:51 +0200

    boats CRUD started

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,22 +1,54 @@
 class BoatsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
+  before_action :set_boat, only: [:edit, :update, :show, :destroy]
 
   def index
+    @boats = Boat.all
   end
 
   def show
+    # see before_action
+  end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    if @boat.save
+      redirect_to show_path(@boat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    # see before_action
+  end
+
+  def update #do we need save method for update
+    @boat.update(boat_params)
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @boat.destroy
+    redirect_to boats_path
+  end
+
+
+
+  private
+
+  def set_boat
+    @boat = Boat.find(params[:id])
+  end
+
+  def boat_params
+    params.require(:boat).permit(:name, :location, :capacity, :description, :price, :boat_type, :user_id)
   end
 end
