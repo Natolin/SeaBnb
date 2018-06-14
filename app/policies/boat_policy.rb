e0f10d08cffa9eb8639b831f6b4591a3bc2d6b78
Piotr Godzinski <hello@piotrgodzinski.com>
2018-06-14 18:26:14 +0200

    pundit boat policy change

---
+++ app/policies/boat_policy.rb
@@ -0,0 +1,18 @@
+class BoatPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+  def create?
+    !user.nil?
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
